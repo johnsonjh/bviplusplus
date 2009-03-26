a85d9dd879bebbdc8b8b8da1485ae5da8e630afd
spacegctc <spacegctc>
2009-03-26 07:05:25 +0000

    Combign windows.c/h into display.c/h
    git-svn-id: https://svn.code.sf.net/p/bviplus/code/branches/myhex@43 f5085f54-095c-457e-827d-aa5372a54925

--- display.c
+++ display.c
@@ -5,6 +5,8 @@
 #include "virt_file.h"
 
 display_info_t display_info;
+WINDOW *window_list[MAX_WINDOWS];
+PANEL *panel_list[MAX_WINDOWS];
 
 void msg_box(char *fmt, ...)
 {
@@ -339,6 +341,116 @@ void print_screen(off_t addr)
     if (address_invalid(line_addr))
       break; /* break after print_line to print the address */
   }
+}
+
+off_t address_invalid(off_t addr)
+{
+  off_t dist_past_range = 0;
+
+  if (addr < 0)
+    dist_past_range = addr;
+  if (addr >= display_info.file_size)
+    dist_past_range = (addr - display_info.file_size) + 1;
+
+  return dist_past_range;
+}
+
+int get_y_from_addr(off_t addr)
+{
+  off_t offset, page_size;
+  int y = 1, z;
+
+  if (addr < display_info.page_start)
+    return -1;
+
+  offset = addr - display_info.page_start;
+  page_size = display_info.page_end - display_info.page_start;
+
+  if (offset > page_size)
+    return -1;
+
+  z = (HEX_COLS * user_prefs[GROUPING].value);
+  y += offset / z;
+
+  return y;
+}
+
+int get_x_from_addr(off_t addr)
+{
+  off_t offset;
+  int x, y;
+  y = get_y_from_addr(addr);
+
+  if (y < 0)
+    return -1;
+
+  y--;
+
+  offset = addr - display_info.page_start;
+  offset -= y * HEX_COLS * user_prefs[GROUPING].value;
+
+  if (display_info.cursor_window == WINDOW_HEX)
+    x = 1 + (offset / user_prefs[GROUPING].value) * BYTES_PER_GROUP;
+  else
+    x = 1 + offset;
+
+  return x;
+
+}
+
+off_t get_addr_from_xy(int x, int y)
+{
+  off_t addr = display_info.page_start;
+
+  if (y < 1 || y > (HEX_BOX_H - 1))
+    return -1;
+
+  if (x < 1 || x > (HEX_BOX_W - 1))
+    return -1;
+
+  y--; /* top border */
+  x--; /* left border */
+
+  if (x > (BYTES_PER_GROUP * HEX_COLS) - BYTES_PER_GROUP)
+    return -1;
+
+  addr += y * HEX_COLS * user_prefs[GROUPING].value;
+  addr += (x / BYTES_PER_GROUP) * user_prefs[GROUPING].value;
+
+  if (addr > display_info.page_end || address_invalid(addr))
+    return -1;
+
+  return addr;
+}
+
+void destroy_screen()
+{
+  delwin(window_list[WINDOW_MENU]);
+  delwin(window_list[WINDOW_ADDR]);
+  delwin(window_list[WINDOW_HEX]);
+  delwin(window_list[WINDOW_ASCII]);
+}
+
+void create_screen()
+{
+  window_list[WINDOW_MENU]  = newwin( MENU_BOX_H,  MENU_BOX_W,  MENU_BOX_Y,  MENU_BOX_X);
+  window_list[WINDOW_ADDR]  = newwin( ADDR_BOX_H,  ADDR_BOX_W,  ADDR_BOX_Y,  ADDR_BOX_X);
+  window_list[WINDOW_HEX]   = newwin(  HEX_BOX_H,   HEX_BOX_W,   HEX_BOX_Y,   HEX_BOX_X);
+  window_list[WINDOW_ASCII] = newwin(ASCII_BOX_H, ASCII_BOX_W, ASCII_BOX_Y, ASCII_BOX_X);
+  window_list[WINDOW_STATUS] = newwin(WINDOW_STATUS_H, WINDOW_STATUS_W, WINDOW_STATUS_Y, WINDOW_STATUS_X);
+
+  panel_list[WINDOW_MENU]  = new_panel(window_list[WINDOW_MENU]);
+  panel_list[WINDOW_ADDR]  = new_panel(window_list[WINDOW_ADDR]);
+  panel_list[WINDOW_HEX]   = new_panel(window_list[WINDOW_HEX]);
+  panel_list[WINDOW_ASCII] = new_panel(window_list[WINDOW_ASCII]);
+  panel_list[WINDOW_STATUS] = new_panel(window_list[WINDOW_STATUS]);
+
+  keypad(window_list[WINDOW_MENU], TRUE);
+  keypad(window_list[WINDOW_HEX], TRUE);
+  keypad(window_list[WINDOW_ASCII], TRUE);
+
+  box(window_list[WINDOW_HEX], 0, 0);
+  box(window_list[WINDOW_ASCII], 0, 0);
 }
 
 
--- display.h
+++ display.h
@@ -4,12 +4,71 @@
 #ifndef __DISPLAY_H__
 #define __DISPLAY_H__
 
+#define MENU_BOX_X    0
+#define MENU_BOX_Y    0
+#define MENU_BOX_W    COLS
+#define MENU_BOX_H    1
+
+#define ADDR_DIGITS   8
+#define ADDR_BOX_X    0
+#define ADDR_BOX_Y    MENU_BOX_H
+#define ADDR_BOX_W    (ADDR_DIGITS + 1)
+#define ADDR_BOX_H_   (LINES - MENU_BOX_H - WINDOW_STATUS_H)
+#define ADDR_BOX_H    (ADDR_BOX_H_ > 3 ? ADDR_BOX_H_ : 3)
+
+#define WINDOW_STATUS_H 1
+#define WINDOW_STATUS_W COLS
+#define WINDOW_STATUS_Y MENU_BOX_H + ADDR_BOX_H
+#define WINDOW_STATUS_X 0
+
+#define BYTE_DIGITS   (user_prefs[DISPLAY_BINARY].value ? 8 : 2)
+
+/* each byte is represented in hex by two digits, plus each grouping has one trailing space */
+#define BYTES_PER_GROUP ((user_prefs[GROUPING].value * BYTE_DIGITS) + 1)
+
+/* width of hex box + ascii box */
+#define SHARED_WIDTH (COLS - ADDR_BOX_W)
+
+#define HEX_BOX_X     (ADDR_BOX_X + ADDR_BOX_W)
+#define HEX_BOX_Y     MENU_BOX_H
+//#define HEX_BOX_W     ((SHARED_WIDTH * BYTES_PER_GROUP) / (BYTES_PER_GROUP + user_prefs.grouping))
+//#define HEX_BOX_W     ((((SHARED_WIDTH - 2) * BYTES_PER_GROUP) + 2 * user_prefs[GROUPING].value) / (BYTES_PER_GROUP + user_prefs[GROUPING].value))
+#define HEX_BOX_W_    ((((SHARED_WIDTH - 2) * BYTES_PER_GROUP) + 2 * user_prefs[GROUPING].value) / (BYTES_PER_GROUP + user_prefs[GROUPING].value))
+#define HEX_BOX_W     (HEX_BOX_W_ > (BYTES_PER_GROUP + 2) ? HEX_BOX_W_ : (BYTES_PER_GROUP + 2))
+#define HEX_BOX_H     ADDR_BOX_H
+
+#define ASCII_BOX_X   (HEX_BOX_X + HEX_BOX_W)
+#define ASCII_BOX_Y   MENU_BOX_H
+#define ASCII_BOX_W_  (SHARED_WIDTH - HEX_BOX_W)
+#define ASCII_BOX_W   (ASCII_BOX_W_ > (user_prefs[GROUPING].value + 2) ? ASCII_BOX_W_ : (user_prefs[GROUPING].value + 2))
+#define ASCII_BOX_H   ADDR_BOX_H
+
+//#define MAX_HEX_COLS    ((HEX_BOX_W - 3) / BYTES_PER_GROUP)
+#define MAX_HEX_COLS    ((HEX_BOX_W - 2) / BYTES_PER_GROUP)
+#define HEX_COLS        (user_prefs[MAX_COLS].value == 0 ? MAX_HEX_COLS : MAX_HEX_COLS > user_prefs[MAX_COLS].value ? user_prefs[MAX_COLS].value : MAX_HEX_COLS)
+#define BYTES_PER_LINE  (HEX_COLS * user_prefs[GROUPING].value)
+#define HEX_LINES       (HEX_BOX_H - 2)
+
+#define PAGE_SIZE ((HEX_LINES * BYTES_PER_LINE) - 1)
+#define _PAGE_END (display_info.page_start + PAGE_SIZE)
+#define PAGE_END  (_PAGE_END > display_info.file_size ? display_info.file_size : _PAGE_END)
+
 #define HEX(x) ((x) < 0xA ? '0' + (x) : 'a' + (x) - 0xa)
 #define MSG_BOX_H 8
 #define MSG_BOX_W 50
 #define MSG_BOX_Y (((HEX_BOX_H - MSG_BOX_H) / 2) + HEX_BOX_Y)
 #define MSG_BOX_X (((HEX_BOX_W - MSG_BOX_W) / 2) + HEX_BOX_X)
 #define MAX_MSG_BOX_LEN ((MSG_BOX_H - 3) * (MSG_BOX_W - 2))
+
+typedef enum window_e
+{
+  WINDOW_MENU,
+  WINDOW_ADDR,
+  WINDOW_HEX,
+  WINDOW_ASCII,
+  WINDOW_STATUS,
+  MAX_WINDOWS
+} window_t;
 
 typedef struct display_info_s
 {
@@ -39,9 +98,16 @@ typedef enum cursor_e
 } cursor_t;
 
 
-
 extern display_info_t display_info;
+extern WINDOW *window_list[MAX_WINDOWS];
+extern PANEL *panel_list[MAX_WINDOWS];
 
+off_t address_invalid(off_t addr);
+int get_y_from_addr(off_t addr);
+int get_x_from_addr(off_t addr);
+off_t get_addr_from_xy(int x, int y);
+void destroy_screen();
+void create_screen();
 void msg_box(char *fmt, ...);
 void reset_display_info(void);
 void update_display_info(void);
