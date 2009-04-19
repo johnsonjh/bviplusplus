90a397bc34f388acd5ee3a8ae320ac19c5b96718
spacegctc <spacegctc>
2009-04-19 09:41:10 +0000

    Fix potential crash when displaying current settings.
    git-svn-id: https://svn.code.sf.net/p/bviplus/code/trunk@119 f5085f54-095c-457e-827d-aa5372a54925

--- help.c
+++ help.c
@@ -196,7 +196,7 @@ void scrollable_window_display(char **text)
       mvwprintw(scrollbox, SCROLL_BOX_H - 2, 1, " [j|DOWN] Down  [k|UP] Up  [q|ESC] Quit |");
       wrefresh(scrollbox);
     }
-    c = getch();
+    c = wgetch(scrollbox);
   } while(c != 'q' && c != ESC);
 
   delwin(scrollbox);
